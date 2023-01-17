3a106896ff4d09378749f56bf1049708da42c0b2
gilgu <gilmich@post.bgu.ac.il>
2023-01-17 12:22:02 +0200

    THIS IS THE PROJECT WE ARE WORKING ON!!! SDK 14.0.1 java 18

--- Cucumber/src/test/java/hellocucumber/StepDefinitions.java
+++ Cucumber/src/test/java/hellocucumber/StepDefinitions.java
@@ -4,11 +4,28 @@ import io.cucumber.java.en.*;
 
 import org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.List;
 public class StepDefinitions {
+    private static List<PrestaActuator> allPrestas;
+    private static PrestaActuator presta;
+    private String webDriver = "webdriver.chrome.driver";
+    private String path = "C:\\Users\\gilgu\\Desktop\\הנדסת מערכות מידע\\סמסטר ה'\\הנדסת איכות תוכנה\\sqe-hw3-main\\sqe-hw3-main\\sqe-hw3\\Selenium\\chromedriver.exe";
+    private void prestaInit(){
+        System.out.println("--------------- INITIALIZING MOODLE TEST - OPENING WEBPAGE ---------------");
+        if(allPrestas == null){
+            allPrestas = new ArrayList<>();
+        }
+        presta = new PrestaActuator();
+        allPrestas.add(presta);
+        presta.initSession(webDriver, path);
+    }
 
-    // $$*TODO* explain what this step does$$
-    @Given("an example scenario")
-    public void anExampleScenario() {
+    @Given("User is on the catalog")
+    public void userIsOnTheCatalog() {
+        prestaInit();
+        presta.InitAdmin();
+
     }
 
     // $$*TODO* explain what this step does$$
@@ -21,4 +38,39 @@ public class StepDefinitions {
     public void theScenarioPasses() {
     }
 
+    @Then("message displayed item out of stock!")
+    public void messageDisplayedItemOutOfStock() {
+    }
+
+    @When("User adds item to cart")
+    public void userAddsItemToCart() {
+        
+    }
+
+    @And("Admin set item stock to {int}")
+    public void adminSetItemStockTo(int arg0) {
+    }
+
+    @When("User adds item on discount to cart")
+    public void userAddsItemOnDiscountToCart() {
+        
+    }
+
+    @And("Admin changes discount")
+    public void adminChangesDiscount() {
+        
+    }
+
+    @Then("message displayed price changed!")
+    public void messageDisplayedPriceChanged() {
+    }
+
+    @And("User returns to shopping")
+    public void userReturnsToShopping() {
+        
+    }
+
+    @And("user returns to shopping cart")
+    public void userReturnsToShoppingCart() {
+    }
 }
--- Cucumber/src/test/resources/hellocucumber/example.feature
+++ Cucumber/src/test/resources/hellocucumber/example.feature
@@ -1,6 +1,26 @@
-Feature: A set of scenarios for testing the "example" module
+Feature: A set of scenarios for testing the Shopping Cart? module
 
-  Scenario: Testing how a case where a user adds a product to the cart
-    Given an example scenario
-    When all step definitions are implemented
-    Then the scenario passes
+  Scenario: User adds item to cart, admin drops stock to 0
+    Given User is on the catalog
+    When User adds item to cart
+    And User returns to shopping
+    And Admin set item stock to 0
+    And user returns to shopping cart
+    Then message displayed item out of stock!
+
+#  user adds item to cart:
+#  clicks on the item , adds to shopping cart with quantity, etc..., returns to catalog
+#
+#  admin sets stock:
+#  admin init,admin login, catalogue -> stock -> find product -> change stock amount to 0
+#
+#  user returns to shopping cart:
+#  click shopping cart.
+#
+#  messege displayed:
+
+#  Scenario: User adds item on discount to cart, admin edit item discount percentage
+#    Given User is on the catalog
+#    When User adds item on discount to cart
+#    And Admin changes discount
+#    Then message displayed price changed!
